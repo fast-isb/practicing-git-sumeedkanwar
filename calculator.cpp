e991e9f5490bbf20687ac5765d0c5a3111ddd1df
sumeedkanwar <sumeedkanwar@gmail.com>
2025-04-05 23:08:41 +0500

    Added subtraction feature

---
+++ calculator.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int subtract(int a, int b)
+{
+    return a - b;
+}
+
+int main()
+{
+    cout << "Basic Calculator" << endl;
+    cout << "2 + 3 = " << add(2, 3) << endl;
+    cout << "5 - 2 = " << subtract(5, 2) << endl;
+    return 0;
+}
