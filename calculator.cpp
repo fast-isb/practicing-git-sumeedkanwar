8202fc6aceeed955f30c8089a7b1771fefca478f
sumeedkanwar <sumeedkanwar@gmail.com>
2025-04-05 23:10:34 +0500

    Added multiplication

--- calculator.cpp
+++ calculator.cpp
@@ -11,10 +11,16 @@ int subtract(int a, int b)
     return a - b;
 }
 
+int multiply(int a, int b)
+{
+    return a * b;
+}
+
 int main()
 {
     cout << "Basic Calculator" << endl;
     cout << "2 + 3 = " << add(2, 3) << endl;
     cout << "5 - 2 = " << subtract(5, 2) << endl;
+    cout << "4 * 3 = " << multiply(4, 3) << endl;
     return 0;
 }
